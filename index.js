707ea1adba973b68c7dfb16827bd64aa1d43d2f5
TimurCherin <109847059+TimurCherin@users.noreply.github.com>
2024-04-09 17:56:58 +0300

    add

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 const wrap = document.querySelector(".wrap")
+const searchInput = document.querySelector(".search_input")
+const page = 1
+let search;
+const moreBtn = document.querySelector(".add")
+let perPage = 12
+let url = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${search}&page=${page}&per_page=${perPage}&key=43135945-0fa309f6e906fbaa5e36dac33`
+function fetchUrl(url){
+    const data = fetch(url)
+    return data
+}
+moreBtn.addEventListener("click",addPages)
+function addPages() {
+    page ++
+    MarkUp(data)
+}
+searchInput.addEventListener("change", onSearch)
+function onSearch(){
+    search = searchInput.value
+    let url = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${search}&page=${page}&per_page=12&key=43135945-0fa309f6e906fbaa5e36dac33`
+    fetchUrl(url).then((response) => response.json()).then((data) => {
+        MarkUp(data.hits)
+    })
+    searchInput.removeEventListener("change", onSearch)
+}
+
+function MarkUp(data){
+    const markUp = data.map((obj) => {
+        return `<img src=${obj.webformatURL}>`
+    })
+    wrap.insertAdjacentHTML("beforeend", markUp.join(""))
+}
