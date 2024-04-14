0e46a1cd053767663b58b9094ff3a714c2357a0c
TimurCherin <109847059+TimurCherin@users.noreply.github.com>
2024-04-14 21:16:27 +0300

    a

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import 'izitoast/dist/css/iziToast.min.css';
 const wrap = document.querySelector(".wrap")
 const searchInput = document.querySelector(".search_input")
 const searchForm = document.querySelector(".search_form")
+const backdrop = document.querySelector(".backdrop")
 let search;
 let page = 1
 const moreBtn = document.querySelector(".add")
@@ -12,7 +13,6 @@ const fetchUrl = async (search, page) => {
     try {
     const response = await fetch(`https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${search}&page=${page}&per_page=${perPage}&key=43135945-0fa309f6e906fbaa5e36dac33`);
     const images = await response.json();
-    console.log(images);
     return images
     } catch (error) {
     console.log(error.message);
@@ -58,7 +58,41 @@ async function onSearch(e){
 }
 function MarkUp(data){
     const markUp = data.map((obj) => {
-        return `<img src=${obj.webformatURL}>`
+        console.log(obj)
+        return `<img alt="${obj.tags}" data-id="${obj.id}" src=${obj.webformatURL}>`
     })
     wrap.insertAdjacentHTML("beforeend", markUp.join(""))
 }
+
+wrap.addEventListener("click", openModal)
+function openModal(e){
+    if(e.target.nodeName === "IMG"){
+        document.querySelector("html").classList.add("no_scroll")
+        const imgId = e.target.dataset.id
+        const obj = fetchImage(imgId)
+        console.log(333,obj)
+        ModalMarkUp(obj)
+    }
+}
+    const fetchImage = async (id) => { 
+        try {
+        const response = await fetch(`https://pixabay.com/api?id=${id}&key=43135945-0fa309f6e906fbaa5e36dac33`);
+        const images = await response.json();
+        console.log("1222",images.hits[0])
+        return images.hits[0]
+        } catch (error) {
+        console.log(error.message);
+        }
+        };
+
+        function ModalMarkUp(obj){
+            const markUp = `
+            <div class="backdrop">
+        <div class="modal">
+            <img class="modal_img" src=${obj.pageURL} alt="${obj.tags}">
+            <p class="photo_description">${obj.tags}</p>
+        </div>
+    </div>
+            `
+            document.querySelector("body").insertAdjacentHTML("beforeend", markUp)
+        }
