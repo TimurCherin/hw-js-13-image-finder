dd51efb65d8ed2ea5f76227402fc3b26d20e87b8
TimurCherin <109847059+TimurCherin@users.noreply.github.com>
2024-05-04 11:26:57 +0300

    final

--- src/index.js
+++ src/index.js
@@ -65,17 +65,14 @@ function MarkUp(data){
 }
 
 wrap.addEventListener("click", openModal)
-function openModal(e){
-    if(e.target.nodeName === "IMG"){
-        document.querySelector("html").classList.add("no_scroll")
-        const imgId = e.target.dataset.id
-        console.log(document.querySelector(".backdrop"))
-        fetchImage(imgId)
-        body.addEventListener("keydown", onEscape)
-        // console.log(document.querySelector(".backdrop"))
-        document.querySelector(".backdrop").addEventListener("click", onBackdrop)
+function openModal(e) {
+    if (e.target.nodeName === 'IMG') {
+      document.querySelector('html').classList.add('no_scroll');
+      const imgId = e.target.dataset.id;
+      fetchImage(imgId);
+      body.addEventListener('keydown', onEscape);
     }
-}
+  }
     const fetchImage = async (id) => { 
         try {
         const response = await fetch(`https://pixabay.com/api?id=${id}&key=43135945-0fa309f6e906fbaa5e36dac33`);
@@ -97,23 +94,24 @@ function openModal(e){
     </div>
             `
             body.insertAdjacentHTML("afterbegin", markUp)
+            document.querySelector(".backdrop").addEventListener("click", onBackdrop)
         }
 
-        function onEscape(e){
-            if(e.code === "Escape"){
-                document.querySelector(".backdrop").remove()
-                body.removeEventListener("keydown", onEscape)
-                document.querySelector("html").classList.remove("no_scroll")
-                body.removeEventListener("keydown", onEscape)
+        function onEscape(e) {
+            if (e.code === 'Escape') {
+              CloseModal();
             }
-        }
-
-        function onBackdrop(e){
-            console.log(e.target.nodeName)
-            if(e.target.nodeName === "backdrop"){
-                document.querySelector(".backdrop").remove()
-                body.removeEventListener("keydown", onEscape)
-                document.querySelector("html").classList.remove("no_scroll")
-                document.querySelector(".backdrop").removeEventListener("click", onBackdrop)
+          }
+          
+          function onBackdrop(e) {
+            if (e.currentTarget === e.target) {
+              CloseModal();
+              e.currentTarget.removeEventListener('click', onBackdrop);
             }
-        }
+          }
+          
+          function CloseModal() {
+            document.querySelector('.backdrop').remove();
+            document.querySelector('html').classList.remove('no_scroll');
+            body.removeEventListener('keydown', onEscape);
+          }
